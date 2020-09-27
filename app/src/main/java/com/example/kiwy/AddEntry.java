44400f9177a3669c1f74fc71875e21959e081729
Kevnaut <kevnaut1997@gmail.com>
2020-09-27 18:56:59 -0400

    Added name changed and attempt save file (not working just yet)

--- app/src/main/java/com/example/kiwy/AddEntry.java
+++ app/src/main/java/com/example/kiwy/AddEntry.java
@@ -2,17 +2,30 @@ package com.example.kiwy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class AddEntry extends AppCompatActivity {
     String btName;
     String btAddress;
+    Button btAddDevice;
+    Button btChangeName;
     Intent intent;
 
     TextView tDevice;
     TextView tAddress;
+    EditText inName;
+
+    public static final String FILE_NAME = "SavedDevices.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +34,55 @@ public class AddEntry extends AppCompatActivity {
         intent = getIntent();
         tDevice = (TextView) findViewById(R.id.tDevice);
         tAddress = (TextView) findViewById(R.id.tAddress);
+        btAddDevice = (Button) findViewById(R.id.btAddDevice);
+        btChangeName = (Button) findViewById(R.id.btChangeName);
+        inName = (EditText) findViewById(R.id.inName);
 
         btName = intent.getStringExtra("btDeviceName");
         btAddress = intent.getStringExtra("btDeviceAddress");
 
         tDevice.setText(btName);
         tAddress.setText(btAddress);
+
+        btAddDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+
+        btChangeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newName = inName.getText().toString();
+                tDevice.setText(newName);
+                btName = newName;
+                inName.setText("");
+            }
+        });
+    }
+
+    public void save() {
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(btName.getBytes());
+            fos.write(btAddress.getBytes());
+
+            Toast.makeText(this, "Devices saved to " + getFilesDir() + "/" + FILE_NAME ,Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
